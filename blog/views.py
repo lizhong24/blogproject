c4fcf6cb19e1d61297a01fb7b06adaa829b65619
lizhong24 <lang812>
2018-05-19 22:37:30 +0800

    Markdown语法和代码高亮

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+import markdown
 
 
 def index(request):
@@ -9,5 +10,11 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
 
