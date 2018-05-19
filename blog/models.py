0d622a444dc028e8160a87d78f17f3fb3848eb78
lizhong24 <lang812>
2018-05-19 19:51:40 +0800

    数据库迁移

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.six import python_2_unicode_compatible
 
 
+# python_2_unicode_compatible 装饰器用于兼容 Python2
+@python_2_unicode_compatible
 class Category(models.Model):
     """
     Django 要求模型必须继承 models.Model 类。
@@ -14,7 +17,11 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
+
+@python_2_unicode_compatible
 class Tag(models.Model):
     """
     标签 Tag 也比较简单，和 Category 一样。
@@ -22,7 +29,11 @@ class Tag(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
+
+@python_2_unicode_compatible
 class Post(models.Model):
     """
     文章的数据库表稍微复杂一点，主要是涉及的字段更多。
@@ -58,3 +69,6 @@ class Post(models.Model):
     # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
